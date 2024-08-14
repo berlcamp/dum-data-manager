b31c4cd3602115d58852a2f734871ca7adb68fe7
Berl treasure Campomanes <berltreasurecampomanes@Berls-MacBook-Pro.local>
2024-08-14 13:55:18 +0800

    ..

--- app/(profiling)/profilingimportexport/Main.tsx
+++ app/(profiling)/profilingimportexport/Main.tsx
@@ -1,7 +1,13 @@
 'use client'
 
 import { Sidebar, Title, TopBar, Unauthorized } from '@/components/index'
-import React, { useState } from 'react'
+import React, { CSSProperties, useState } from 'react'
+
+import {
+  formatFileSize,
+  lightenDarkenColor,
+  useCSVReader,
+} from 'react-papaparse'
 
 import Excel from 'exceljs'
 import { saveAs } from 'file-saver'
@@ -43,12 +49,92 @@ const FormSchema = z.object({
   }),
 })
 
+const GREY = '#CCC'
+const GREY_LIGHT = 'rgba(255, 255, 255, 0.4)'
+const DEFAULT_REMOVE_HOVER_COLOR = '#A01919'
+const REMOVE_HOVER_COLOR_LIGHT = lightenDarkenColor(
+  DEFAULT_REMOVE_HOVER_COLOR,
+  40
+)
+const GREY_DIM = '#686868'
+
+const styles = {
+  zone: {
+    alignItems: 'center',
+    border: `2px dashed ${GREY}`,
+    borderRadius: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'center',
+    padding: 20,
+  } as CSSProperties,
+  file: {
+    background: 'linear-gradient(to bottom, #EEE, #DDD)',
+    borderRadius: 20,
+    display: 'flex',
+    height: 120,
+    width: 120,
+    position: 'relative',
+    zIndex: 10,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  } as CSSProperties,
+  info: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: 10,
+    paddingRight: 10,
+  } as CSSProperties,
+  size: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    marginBottom: '0.5em',
+    justifyContent: 'center',
+    display: 'flex',
+  } as CSSProperties,
+  name: {
+    backgroundColor: GREY_LIGHT,
+    borderRadius: 3,
+    fontSize: 12,
+    marginBottom: '0.5em',
+  } as CSSProperties,
+  progressBar: {
+    bottom: 14,
+    position: 'absolute',
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 10,
+  } as CSSProperties,
+  zoneHover: {
+    borderColor: GREY_DIM,
+  } as CSSProperties,
+  default: {
+    borderColor: GREY,
+  } as CSSProperties,
+  remove: {
+    height: 23,
+    position: 'absolute',
+    right: 6,
+    top: 6,
+    width: 23,
+  } as CSSProperties,
+}
+
 const Page: React.FC = () => {
   //
-
   const { session } = useSupabase()
   const { hasAccess } = useFilter()
   const [downloading, setDownloading] = useState(false)
+
+  // CSV Reader
+  const [csvContents, setCsvContents] = useState(null)
+  const { CSVReader } = useCSVReader()
+  const [zoneHover, setZoneHover] = useState(false)
+  const [removeHoverColor, setRemoveHoverColor] = useState(
+    DEFAULT_REMOVE_HOVER_COLOR
+  )
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -57,6 +143,10 @@ const Page: React.FC = () => {
 
   const onSubmit = async (formdata: z.infer<typeof FormSchema>) => {
     void handleDownloadExcel(formdata)
+  }
+
+  const handleProcessCsvData = async () => {
+    console.log('csvContents', csvContents)
   }
 
   const handleDownloadExcel = async (formdata: z.infer<typeof FormSchema>) => {
@@ -72,6 +162,7 @@ const Page: React.FC = () => {
       { header: 'ID (do not edit)', key: 'id', width: 20 },
       { header: 'Fullname (do not edit)', key: 'fullname', width: 20 },
       { header: 'Category', key: 'category', width: 20 },
+      { header: 'Remarks', key: 'remarks', width: 20 },
       // Add more columns based on your data structure
     ]
 
@@ -104,6 +195,7 @@ const Page: React.FC = () => {
         fullname: `${item.fullname}`,
         barangay: `${item.address}`,
         category: `${category}`,
+        remarks: ``,
       })
     })
 
@@ -222,6 +314,85 @@ const Page: React.FC = () => {
             </div>
             <div className="w-full border-dashed border-2 p-2 bg-gray-50">
               <div className="text-center">Import Data</div>
+              <div className="p-4">
+                <CSVReader
+                  onUploadAccepted={(results: any) => {
+                    console.log('---------------------------')
+                    console.log(results)
+                    setCsvContents(results)
+                    console.log('---------------------------')
+                    setZoneHover(false)
+                  }}
+                  onDragOver={(event: DragEvent) => {
+                    event.preventDefault()
+                    setZoneHover(true)
+                  }}
+                  onDragLeave={(event: DragEvent) => {
+                    event.preventDefault()
+                    setZoneHover(false)
+                  }}>
+                  {({
+                    getRootProps,
+                    acceptedFile,
+                    ProgressBar,
+                    getRemoveFileProps,
+                    Remove,
+                  }: any) => (
+                    <>
+                      <div
+                        {...getRootProps()}
+                        style={Object.assign(
+                          {},
+                          styles.zone,
+                          zoneHover && styles.zoneHover
+                        )}>
+                        {acceptedFile ? (
+                          <>
+                            <div style={styles.file}>
+                              <div style={styles.info}>
+                                <span style={styles.size}>
+                                  {formatFileSize(acceptedFile.size)}
+                                </span>
+                                <span style={styles.name}>
+                                  {acceptedFile.name}
+                                </span>
+                              </div>
+                              <div style={styles.progressBar}>
+                                <ProgressBar />
+                              </div>
+                              <div
+                                {...getRemoveFileProps()}
+                                style={styles.remove}
+                                onMouseOver={(event: Event) => {
+                                  event.preventDefault()
+                                  setRemoveHoverColor(REMOVE_HOVER_COLOR_LIGHT)
+                                }}
+                                onMouseOut={(event: Event) => {
+                                  event.preventDefault()
+                                  setRemoveHoverColor(
+                                    DEFAULT_REMOVE_HOVER_COLOR
+                                  )
+                                }}>
+                                <Remove color={removeHoverColor} />
+                              </div>
+                            </div>
+                          </>
+                        ) : (
+                          'Drop CSV file here or click to upload'
+                        )}
+                      </div>
+                    </>
+                  )}
+                </CSVReader>
+                <div className="mt-2 text-center">
+                  <CustomButton
+                    containerStyles="app__btn_blue"
+                    title="Upload and Import Data"
+                    isDisabled={downloading}
+                    handleClick={handleProcessCsvData}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
